0edc3d676a6ced89a89d570e8d4e2f20f81e492a
Hilton Rocha <hilton-bruno-santos@hotmail.com>
2024-11-16 20:05:23 -0300

    segundo commit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-    return <h1> Teste 1 </h1>
+    return <h1> Tatiane sabia que vc é o amor da minha vida?? Te amo, muito obrigado por estar ao meu lado em todos os momentos, vc é uma pessoa incrivel e tenho muita sorte por ter me escolhido para dividir a vida com vc </h1>
 }
 
 export default Home;
